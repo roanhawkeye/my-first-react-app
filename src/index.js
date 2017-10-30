8e8366703129fee34cd0ee91ce4260c5e44d4c2c
Daniel Martinez <roanhawkeye@gmail.com>
2017-10-30 09:03:55 -0600

    Add moves to navigate them, just the list for now

--- src/index.js
+++ src/index.js
@@ -75,6 +75,18 @@ function Square(props){
       const history = this.state.history;
       const current = history[history.length - 1];
       const winner = calculateWinner(current.squares);
+
+      const moves = history.map((step, move) => {
+          const desc = move ? 
+            'Go to move #' + move :
+            'Go to game start';
+          return (
+              <li>
+                  <button onClick={ () => this.jumpTo(move)}>{desc}</button>
+              </li>
+          );
+      })
+
       
       let status;
       if(winner){
@@ -93,7 +105,7 @@ function Square(props){
           </div>
           <div className="game-info">
             <div>{status}</div>
-            <ol>{/* TODO */}</ol>
+            <ol>{moves}</ol>
           </div>
         </div>
       );
